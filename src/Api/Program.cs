f43e5583d6992673023fab20d6b2e8feff7e270e
Elmer Marquez <leomarqz.main@gmail.com>
2026-02-08 13:38:42 -0600

    Se dio inicio a la configuracion relacionada con identity, jwt y cors.

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,11 +1,18 @@
 
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using XNerd.Ecommerce.Domain.Models;
 using XNerd.Ecommerce.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,6 +35,50 @@ builder.Services.AddControllers((options) =>
     var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
     options.Filters.Add( new AuthorizeFilter(policy) );
 });
+
+IdentityBuilder identityBuilder = builder.Services.AddIdentityCore<User>();
+identityBuilder = new IdentityBuilder(identityBuilder.UserType, identityBuilder.Services);
+
+// Soporte para los roles y seguridad con tokens
+identityBuilder.AddRoles<IdentityRole>().AddDefaultTokenProviders();
+
+// Soporte para claims - son las propiedades que se pueden agregar dentro del token (jwt)
+identityBuilder.AddClaimsPrincipalFactory<UserClaimsPrincipalFactory<User, IdentityRole>>();
+
+// Soporte del identity builder en la base de datos (tablas)
+identityBuilder.AddEntityFrameworkStores<EcommerceDbContext>();
+
+// soporte para el login
+identityBuilder.AddSignInManager<SignInManager<User>>();
+
+// Soporte para la creacion de los datetimes
+builder.Services.TryAddSingleton<ISystemClock, SystemClock>();
+
+// Json Web Token
+var key = new SymmetricSecurityKey( Encoding.UTF8.GetBytes( builder.Configuration["JwtSettings:Key"]! ) );
+
+builder.Services.AddAuthentication( JwtBearerDefaults.AuthenticationScheme )
+    .AddJwtBearer((options) =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = key,
+            ValidateAudience = false,
+            ValidateIssuer = false
+        };
+    });
+
+builder.Services.AddCors((options) =>
+{
+    options.AddPolicy("CorsPolicy", (builder) =>
+    {
+        builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -45,6 +96,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
