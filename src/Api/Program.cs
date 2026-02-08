30940f413a440d40c49a565aa43c048c0737bc0c
Elmer Marquez <leomarqz.main@gmail.com>
2026-02-08 00:43:02 -0600

    Configurando cadena de conexion a base de datos sin usuario y contrasenia. inicios de la configuracion relacionada con la seguridad de la api

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,14 +1,33 @@
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using XNerd.Ecommerce.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//---------------------------------------------------------------------
+builder.Services.AddDbContext<EcommerceDbContext>((options) =>
+{
+    var cnnStr = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(cnnStr, (others) =>
+    {   // Mostrara las consultas sql ejecutadas por consola
+        others.MigrationsAssembly( typeof(EcommerceDbContext).Assembly.FullName );
+    });
+});
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
-builder.Services.AddControllers();
+
+builder.Services.AddControllers((options) =>
+{   // Esto obliga a que todos los endpoints requieran un token valido
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    options.Filters.Add( new AuthorizeFilter(policy) );
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,8 +41,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
